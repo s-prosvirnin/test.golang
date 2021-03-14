28643c87cb9deaa43aea1eec2fef217af772875a
s.prosvirnin <s.prosvirnin@ya.ru>
2021-03-14 21:32:21 +0300

    tasks

--- tickets_and_change.go
+++ tickets_and_change.go
@@ -2,17 +2,49 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func isEnoughChangeForTickets(moneyForTickets []int) string {
 
-	currentChange := 0
 	const ticketCost = 25
+	isEnoughChange := true
+	dollarsBillsMap := map[int]int {
+		100: 0,
+		50: 0,
+		25: 0,
+	}
 	for _, moneyForTicket := range moneyForTickets {
-		if moneyForTicket - ticketCost
+		change := moneyForTicket - ticketCost
+		if change > 0 {
+			for _, denomination := range []int{100, 50, 25} {
+				billsCount := dollarsBillsMap[denomination]
+				if change >= denomination && billsCount > 0 {
+					changeBillsCount := int(math.Floor(float64(change) / float64(denomination)))
+					if changeBillsCount >= billsCount {
+						dollarsBillsMap[denomination] = 0
+						change -= billsCount * denomination
+					} else {
+						dollarsBillsMap[denomination] -= changeBillsCount
+						change -= changeBillsCount * denomination
+					}
+				}
+			}
+		}
+
+		if change > 0 {
+			isEnoughChange = false
+			break
+		}
+
+		dollarsBillsMap[moneyForTicket]++
 	}
 
-	return "NO"
+	if isEnoughChange == false {
+		return "NO"
+	}
+
+	return "YES"
 }
 
 func doTestIsEnoughChangeForTickets(moneyForTickets []int, expectedResult bool) {
@@ -32,6 +64,10 @@ func doTestIsEnoughChangeForTickets(moneyForTickets []int, expectedResult bool) 
 
 func main() {
 	doTestIsEnoughChangeForTickets(
+		[]int{25, 25, 25, 25, 50, 100, 50},
+		true,
+	)
+	doTestIsEnoughChangeForTickets(
 		[]int{25, 25, 50},
 		true,
 	)
@@ -40,7 +76,31 @@ func main() {
 		false,
 	)
 	doTestIsEnoughChangeForTickets(
+		[]int{50},
+		false,
+	)
+	doTestIsEnoughChangeForTickets(
 		[]int{25, 25, 50, 50, 100},
 		false,
+	)
+	doTestIsEnoughChangeForTickets(
+		[]int{},
+		true,
+	)
+	doTestIsEnoughChangeForTickets(
+		[]int{25, 25, 50, 50, 25, 100, 25, 25, 50, 50},
+		true,
+	)
+	doTestIsEnoughChangeForTickets(
+		[]int{25, 25, 50, 50, 25, 100, 25, 25, 50, 50, 50},
+		false,
+	)
+	doTestIsEnoughChangeForTickets(
+		[]int{25, 25, 25, 25, 50, 100, 50},
+		true,
+	)
+	doTestIsEnoughChangeForTickets(
+		[]int{25, 25, 25, 25, 50, 100, 50, 25, 100, 25, 50},
+		true,
 	)
 }
